8bb0294362b6066ec6702f9f321aae865fe91e6b
AlbanChatelet <chateletalban20@gmail.com>
2026-02-24 17:58:26 +0100

    Initial Nuxt 3 setup - PharmaInsight base project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>PharmaInsight</h1>
+    <p>Internal analytics dashboard for pharmacy performance.</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 40px;
+  font-family: sans-serif;
+}
+</style>
